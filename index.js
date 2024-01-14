58b5fa80f1fd46e1d980c0f5cf7033ad6f5061d5
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-01-14 14:58:56 -0800

    numerical lab complete

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+};
+
+function subtract() {
+};
+
+function multiply() {
+};
+
+function divide() {
+};
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+number = 15
+
+function increment(number) {
+    return (number += 1);
+}
+
+number = 20
+
+function decrement(number) {
+    return (number -= 1);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(P) {
+    return parseFloat(P);
+}
